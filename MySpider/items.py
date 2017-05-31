3baa7c0f0332a7ce3a209f38660f10867a59cc3c
ShirokoSama <141250108@smail.nju.edu.cn>
2017-05-31 16:45:44 +0800

    格瓦拉的，然而第一次测试就被Ban了IP

---
+++ MySpider/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class MyspiderItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    pass
+
+class GewaraFilm(scrapy.Item):
+    name = scrapy.Field()
+    cinema = scrapy.Field()
+    date = scrapy.Field()
+    screenings = scrapy.Field()
+
+class GewaraFilmScreenings(scrapy.Item):
+    auditorium = scrapy.Field()
+    time = scrapy.Field()
+    price = scrapy.Field()
+    language = scrapy.Field()
+
---
+++ MySpider/spiders/gewara_spider.py
@@ -0,0 +1,49 @@
+import scrapy
+from MySpider.items import GewaraFilmScreenings
+from MySpider.items import GewaraFilm
+
+class GewaraSpider(scrapy.Spider):
+    name = 'gewara'
+    start_urls = ['http://www.gewara.com/cinema/searchOpi.xhtml']
+
+    def start_requests(self):
+        return [scrapy.FormRequest("http://www.gewara.com/cinema/searchOpi.xhtml",
+                                   cookies={'citycode': '320100'},
+                                   callback=self.parse_city)]
+
+    def parse_city(self, response):
+        prefix = 'http://www.gewara.com/movie/ajax/getOpiItemNew.xhtml'
+        movie_id_list = response.xpath('//div[@id="movieSiteHeight"]/a/@id').extract()
+        movie_name_list = response.xpath('//div[@id="movieSiteHeight"]/a/text()').extract()
+        cinema_id_list = response.xpath('//div[@id="cinemaPanel"]/a/@id').extract()
+        cinema_name_list = response.xpath('//div[@id="cinemaPanel"]/a/text()').extract()
+        date_list = response.xpath('//a[@rel="fyrq"]/@id').extract()
+        for fyrq in date_list:
+            for cid, cinema_name in zip(cinema_id_list, cinema_name_list):
+                for movieid, movie_name in zip(movie_id_list, movie_name_list):
+                    ajax_url = prefix + '?cid=' + cid + '&movieid=' + movieid + '&fyrq=' + fyrq
+                    date = fyrq
+                    name = movie_name.strip()
+                    cinema = cinema_name.strip()
+                    yield scrapy.Request(ajax_url, callback=lambda response, date=date, name=name, cinema=cinema: self.parse_ajax)
+
+    def parse_ajax(self, response, date, name, cinema):
+        screenings = []
+        chooseOpi_body = response.xpath('//div[@class="chooseOpi_body"]/ul/li')
+        for li in enumerate(chooseOpi_body):
+            auditorium = li.xpath('//span[@class="opiRoom ui_roomType"]/label/text()').extract_first()
+            time = li.xpath('//span[@class="opitime"]/b/text()').extract_first()
+            price = li.xpath('//span[@class="opiPrice"]/b/text()').extract_first()
+            language = li.xpath('//span[@class="opiEdition"]/em/text()').extract_first()
+            screening = GewaraFilmScreenings(auditorium=auditorium, time=time, price=price, language=language)
+            screenings.append(screening)
+        film = GewaraFilm(name=name, cinema=cinema, date=date, screenings=screenings)
+        return film
+
+
+
+
+
+
+
+
